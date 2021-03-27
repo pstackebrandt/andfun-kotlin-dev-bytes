bda562efe1065f4a07b33f216d70918756533433
Peter Stackebrandt <peter.stackebrandt@immowelt.de>
2021-03-27 17:48:26 +0100

    Create and get Database

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,12 +17,19 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
+/**
+ * Stores a collection of Database videos in a room database.
+ */
 @Dao
 interface VideoDao {
     @Query("select * from databasevideo")
@@ -30,4 +37,22 @@ interface VideoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,
+                "videos").build()
+        }
+    }
+    return INSTANCE
 }
